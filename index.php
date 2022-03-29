1fa577135c492dcd1d0a5b179cb9019611be8ad3
San <sani4.mocrousov@yandex.ru>
2022-03-29 10:41:25 +0300

    фыв

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Тайтл</title>
+</head>
+
+<body></body>
+<header>
+	Шапка сайта
+</header>
+
+<?php
+echo '<hr><hr><br><h2>Модуль №7: Шаблоны и стандарты проектирования
+</h2>' . "<br>";
+echo '<hr><hr><br><h3>Back - #7.1-8 - Введение в шаблоны</h3><hr><hr>' . "<br>";
+/*Введение:
+
+*/
+
+?>
+<footer>Подвал сайта</footer>
+</body>
+
+</html>
